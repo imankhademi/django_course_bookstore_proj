1e881e9fa8b71eb5e17886a1bf069c31ed4e67a1
iman khademi <pouyasharr66@gmail.com>
2023-01-05 13:42:34 +0330

    add tests

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-urlpatterns = {
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
-}
+]
